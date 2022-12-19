c3cfc27f9c87b9d0d83de3ba5325178ee2491239
David Hook <david.hook@keyfactor.com>
2022-12-19 14:54:13 +1100

    added direct parsing for directories.

--- providers/bc/src/main/java/ArtifactParser.java
+++ providers/bc/src/main/java/ArtifactParser.java
@@ -1,5 +1,8 @@
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.security.KeyFactory;
@@ -29,6 +32,7 @@ import org.bouncycastle.util.io.pem.PemReader;
 public class ArtifactParser
 {
     private static Map<String, Map<String, ZipEntry>> algEntries = new HashMap<>();
+    private static Map<String, Map<String, File>> fileAlgEntries = new HashMap<>();
 
     private static boolean checkCertificate(String entryName, X509Certificate subject, X509Certificate signingCert)
     {
@@ -60,10 +64,10 @@ public class ArtifactParser
         }
     }
 
-    private static void checkCSR(ZipFile zipFile, ZipEntry zipEntry, Set<String> passed)
+    private static void checkCSR(String csrName, InputStream csrStream, Set<String> passed)
         throws IOException
     {
-        byte[] enc = Streams.readAll(zipFile.getInputStream(zipEntry));
+        byte[] enc = Streams.readAll(csrStream);
         if (enc[0] != 0x30)
         {
             PemReader pemReader = new PemReader(new InputStreamReader(new ByteArrayInputStream(enc)));
@@ -76,16 +80,16 @@ public class ArtifactParser
             PKCS10CertificationRequest certRequest = new JcaPKCS10CertificationRequest(enc);
             if (certRequest.isSignatureValid(new JcaContentVerifierProviderBuilder().build(certRequest.getSubjectPublicKeyInfo())))
             {
-                passed.add(zipEntry.getName());
+                passed.add(csrName);
             }
             else
             {
-                System.err.println("Entry " + zipEntry.getName() + " failed to verify");
+                System.err.println("Entry " + csrName + " failed to verify");
             }
         }
         catch (Exception e)
         {
-            System.err.println("Entry " + zipEntry.getName() + " failed to parse: " + e.getMessage());
+            System.err.println("Entry " + csrName + " failed to parse: " + e.getMessage());
         }
     }
 
@@ -107,21 +111,12 @@ public class ArtifactParser
         return name.endsWith(".pem") || name.endsWith("der");
     }
 
-    public static void main(String[] args)
+    public static void processZipArtifacts(String zipName)
         throws Exception
     {
-        if (args.length != 1)
-        {
-            System.err.println("usage: ArtifactParser artifacts.zip");
-            System.exit(1);
-        }
+        ZipFile zipFile = new ZipFile(zipName);
 
-        Security.addProvider(new BouncyCastleProvider());
-        Security.addProvider(new BouncyCastlePQCProvider());
-
-        ZipFile zipFile = new ZipFile(args[0]);
-
-        for (Enumeration<? extends ZipEntry> en = zipFile.entries(); en.hasMoreElements();)
+        for (Enumeration<? extends ZipEntry> en = zipFile.entries(); en.hasMoreElements(); )
         {
             ZipEntry entry = en.nextElement();
             String[] entryTokens = entry.getName().split("/");
@@ -195,16 +190,23 @@ public class ArtifactParser
                 }
                 continue;
             }
-            for (String name: algEntry.keySet())
+            for (String name : algEntry.keySet())
             {
                 ZipEntry zipEntry = algEntry.get(name);
                 if (name.startsWith("ca/"))
                 {
                     if (name.contains("pub") && isRecognizedEncoding(name))
                     {
-                        PemReader pemReader = new PemReader(new InputStreamReader(zipFile.getInputStream(zipEntry)));
-                        taPub = KeyFactory.getInstance(oid).generatePublic(new X509EncodedKeySpec(pemReader.readPemObject().getContent()));
-                        if (taPub != null)
+                        if (name.endsWith("pem"))
+                        {
+                            PemReader pemReader = new PemReader(new InputStreamReader(zipFile.getInputStream(zipEntry)));
+                            caPub = KeyFactory.getInstance(oid).generatePublic(new X509EncodedKeySpec(pemReader.readPemObject().getContent()));
+                        }
+                        else
+                        {
+                            caPub = KeyFactory.getInstance(oid).generatePublic(new X509EncodedKeySpec(Streams.readAll(zipFile.getInputStream(zipEntry))));
+                        }
+                        if (caPub != null)
                         {
                             passed.add(zipEntry.getName());
                         }
@@ -216,7 +218,7 @@ public class ArtifactParser
                     }
                     else if (name.endsWith("csr"))
                     {
-                        checkCSR(zipFile, zipEntry, passed);
+                        checkCSR(zipEntry.getName(), zipFile.getInputStream(zipEntry), passed);
                     }
                     else
                     {
@@ -227,8 +229,15 @@ public class ArtifactParser
                 {
                     if (name.contains("pub") && isRecognizedEncoding(name))
                     {
-                        PemReader pemReader = new PemReader(new InputStreamReader(zipFile.getInputStream(zipEntry)));
-                        taPub = KeyFactory.getInstance(oid).generatePublic(new X509EncodedKeySpec(pemReader.readPemObject().getContent()));
+                        if (name.endsWith("pem"))
+                        {
+                            PemReader pemReader = new PemReader(new InputStreamReader(zipFile.getInputStream(zipEntry)));
+                            taPub = KeyFactory.getInstance(oid).generatePublic(new X509EncodedKeySpec(pemReader.readPemObject().getContent()));
+                        }
+                        else
+                        {
+                            taPub = KeyFactory.getInstance(oid).generatePublic(new X509EncodedKeySpec(Streams.readAll(zipFile.getInputStream(zipEntry))));
+                        }
                         if (taPub != null)
                         {
                             passed.add(zipEntry.getName());
@@ -248,7 +257,7 @@ public class ArtifactParser
                     }
                     else if (name.endsWith("csr"))
                     {
-                        checkCSR(zipFile, zipEntry, passed);
+                        checkCSR(zipEntry.getName(), zipFile.getInputStream(zipEntry), passed);
                     }
                     else
                     {
@@ -260,7 +269,14 @@ public class ArtifactParser
                     if (name.contains("pub") && isRecognizedEncoding(name))
                     {
                         PemReader pemReader = new PemReader(new InputStreamReader(zipFile.getInputStream(zipEntry)));
-                        eePub = KeyFactory.getInstance(oid).generatePublic(new X509EncodedKeySpec(pemReader.readPemObject().getContent()));
+                        if (name.endsWith("pem"))
+                        {
+                            eePub = KeyFactory.getInstance(oid).generatePublic(new X509EncodedKeySpec(pemReader.readPemObject().getContent()));
+                        }
+                        else
+                        {
+                            eePub = KeyFactory.getInstance(oid).generatePublic(new X509EncodedKeySpec(Streams.readAll(zipFile.getInputStream(zipEntry))));
+                        }
                         if (eePub != null)
                         {
                             passed.add(zipEntry.getName());
@@ -273,7 +289,7 @@ public class ArtifactParser
                     }
                     else if (name.endsWith("csr"))
                     {
-                        checkCSR(zipFile, zipEntry, passed);
+                        checkCSR(zipEntry.getName(), zipFile.getInputStream(zipEntry), passed);
                     }
                     else
                     {
@@ -357,6 +373,296 @@ public class ArtifactParser
             System.out.println("Checked: " + entry);
             System.out.println("    Passed : " + getMatching(passed, entry));
             System.out.println("    Ignored: " + getMatching(ignored, entry));
+        }
+    }
+
+    public static void processArtifacts(String dirName)
+        throws Exception
+    {
+        File artDir = new File(dirName);
+        if (!artDir.isDirectory())
+        {
+            throw new IllegalStateException("artifact argument must point to a directory");
+        }
+
+        for (File f : artDir.listFiles())
+        {
+            String[] entryTokens = f.getName().split("/");
+            if (entryTokens.length == 4)
+            {
+                Map<String, File> algEntry = fileAlgEntries.get(entryTokens[1]);
+                if (algEntry == null)
+                {
+                    algEntry = new HashMap<>();
+                    fileAlgEntries.put(entryTokens[1], algEntry);
+                }
+
+                algEntry.put(entryTokens[2] + "/" + entryTokens[3], f);
+            }
+            // some zips missing the artifacts directory
+            else if (!entryTokens[0].equals("artifacts") && entryTokens.length == 3)
+            {
+                Map<String, File> algEntry = fileAlgEntries.get(entryTokens[0]);
+                if (algEntry == null)
+                {
+                    algEntry = new HashMap<>();
+                    fileAlgEntries.put(entryTokens[0], algEntry);
+                }
+
+                algEntry.put(entryTokens[1] + "/" + entryTokens[2], f);
+            }
+        }
+
+        Set<String> entriesChecked = new HashSet<>();
+        Set<String> ignored = new HashSet<>();
+        Set<String> passed = new HashSet<>();
+
+        String taName = null;
+        X509Certificate taCert = null;
+        PublicKey taPub = null;
+        String caName = null;
+        X509Certificate caCert = null;
+        PublicKey caPub = null;
+        String eeName = null;
+        X509Certificate eeCert = null;
+        PublicKey eePub = null;
+        String taCrlName = null;
+        X509CRL taCrl = null;
+        String caCrlName = null;
+        X509CRL caCrl = null;
+        CertificateFactory certFact = CertificateFactory.getInstance("X.509", "BC");
+
+        for (String oid : algEntries.keySet())
+        {
+            taName = null;
+            taCert = null;
+            taPub = null;
+            caName = null;
+            caCert = null;
+            caPub = null;
+            eeName = null;
+            eeCert = null;
+            eePub = null;
+            taCrl = null;
+            taCrlName = null;
+            caCrl = null;
+            caCrlName = null;
+
+            Map<String, File> algEntry = fileAlgEntries.get(oid);
+            // ignore SPHINCS+ for now
+            if (oid.startsWith("1.3.9999.6"))
+            {
+                for (File fe : algEntry.values())
+                {
+                    ignored.add(fe.getName());
+                }
+                continue;
+            }
+            for (String name : algEntry.keySet())
+            {
+                File fileEntry = algEntry.get(name);
+                if (name.startsWith("ca/"))
+                {
+                    if (name.contains("pub") && isRecognizedEncoding(name))
+                    {
+                        if (name.endsWith("pem"))
+                        {
+                            PemReader pemReader = new PemReader(new InputStreamReader(new FileInputStream(fileEntry)));
+                            caPub = KeyFactory.getInstance(oid).generatePublic(new X509EncodedKeySpec(pemReader.readPemObject().getContent()));
+                        }
+                        else
+                        {
+                            caPub = KeyFactory.getInstance(oid).generatePublic(new X509EncodedKeySpec(Streams.readAll(new FileInputStream(fileEntry))));
+                        }
+                        if (caPub != null)
+                        {
+                            passed.add(fileEntry.getName());
+                        }
+                    }
+                    else if (!name.contains("priv") && isRecognizedEncoding(name))
+                    {
+                        caName = fileEntry.getName();
+                        caCert = (X509Certificate)certFact.generateCertificate(new FileInputStream(fileEntry));
+                    }
+                    else if (name.endsWith("csr"))
+                    {
+                        checkCSR(fileEntry.getName(), new FileInputStream(fileEntry), passed);
+                    }
+                    else
+                    {
+                        ignored.add(fileEntry.getName());
+                    }
+                }
+                else if (name.startsWith("ta/"))
+                {
+                    if (name.contains("pub") && isRecognizedEncoding(name))
+                    {
+                        if (name.endsWith("pem"))
+                        {
+                            PemReader pemReader = new PemReader(new InputStreamReader(new FileInputStream(fileEntry)));
+                            taPub = KeyFactory.getInstance(oid).generatePublic(new X509EncodedKeySpec(pemReader.readPemObject().getContent()));
+                        }
+                        else
+                        {
+                            taPub = KeyFactory.getInstance(oid).generatePublic(new X509EncodedKeySpec(Streams.readAll(new FileInputStream(fileEntry))));
+                        }
+                        if (taPub != null)
+                        {
+                            passed.add(fileEntry.getName());
+                        }
+                    }
+                    else if (!name.contains("priv") && isRecognizedEncoding(name))
+                    {
+                        try
+                        {
+                            taName = fileEntry.getName();
+                            taCert = (X509Certificate)certFact.generateCertificate(new FileInputStream(fileEntry));
+                        }
+                        catch (Exception e)
+                        {
+                            System.err.println("exception parsing " + fileEntry.getName() + " :" + e.getMessage());
+                        }
+                    }
+                    else if (name.endsWith("csr"))
+                    {
+                        checkCSR(fileEntry.getName(), new FileInputStream(fileEntry), passed);
+                    }
+                    else
+                    {
+                        ignored.add(fileEntry.getName());
+                    }
+                }
+                else if (name.startsWith("ee/"))
+                {
+                    if (name.contains("pub") && isRecognizedEncoding(name))
+                    {
+                        if (name.endsWith("pem"))
+                        {
+                            PemReader pemReader = new PemReader(new InputStreamReader(new FileInputStream(fileEntry)));
+                            eePub = KeyFactory.getInstance(oid).generatePublic(new X509EncodedKeySpec(pemReader.readPemObject().getContent()));
+                        }
+                        else
+                        {
+                            eePub = KeyFactory.getInstance(oid).generatePublic(new X509EncodedKeySpec(Streams.readAll(new FileInputStream(fileEntry))));
+                        }
+                        if (eePub != null)
+                        {
+                            passed.add(fileEntry.getName());
+                        }
+                    }
+                    else if (!name.contains("priv") && isRecognizedEncoding(name))
+                    {
+                        eeName = fileEntry.getName();
+                        eeCert = (X509Certificate)certFact.generateCertificate(new FileInputStream(fileEntry));
+                    }
+                    else if (name.endsWith("csr"))
+                    {
+                        checkCSR(fileEntry.getName(), new FileInputStream(fileEntry), passed);
+                    }
+                    else
+                    {
+                        ignored.add(fileEntry.getName());
+                    }
+                }
+                else if (name.startsWith("crl/"))
+                {
+                    if (name.contains("crl_ta"))
+                    {
+                        try
+                        {
+                            taCrlName = fileEntry.getName();
+                            taCrl = (X509CRL)certFact.generateCRL(new FileInputStream(fileEntry));
+                        }
+                        catch (Exception e)
+                        {
+                            System.err.println("exception parsing " + fileEntry.getName() + " :" + e.getMessage());
+                        }
+                    }
+                    else if (name.contains("crl_ca"))
+                    {
+                        try
+                        {
+                            caCrlName = fileEntry.getName();
+                            caCrl = (X509CRL)certFact.generateCRL(new FileInputStream(fileEntry));
+                        }
+                        catch (Exception e)
+                        {
+                            System.err.println("exception parsing " + fileEntry.getName() + " :" + e.getMessage());
+                        }
+                    }
+                    else
+                    {
+                        ignored.add(fileEntry.getName());
+                    }
+                }
+            }
+
+            if (taCert != null)
+            {
+                if (checkCertificate(taName, taCert, taCert))
+                {
+                    passed.add(taName);
+                }
+            }
+            if (caCert != null && taCert != null)
+            {
+                if (checkCertificate(caName, caCert, taCert))
+                {
+                    passed.add(caName);
+                }
+            }
+            if (eeCert != null && caCert != null)
+            {
+                if (checkCertificate(eeName, eeCert, caCert))
+                {
+                    passed.add(eeName);
+                }
+            }
+            if (taCrl != null && taCert != null)
+            {
+                if (checkCRL(taCrlName, taCrl, taCert))
+                {
+                    passed.add(eeName);
+                }
+            }
+            if (caCrl != null && caCert != null)
+            {
+                if (checkCRL(caCrlName, caCrl, caCert))
+                {
+                    passed.add(eeName);
+                }
+            }
+
+            entriesChecked.add(oid);
+        }
+
+        for (String entry : entriesChecked)
+        {
+            System.out.println("Checked: " + entry);
+            System.out.println("    Passed : " + getMatching(passed, entry));
+            System.out.println("    Ignored: " + getMatching(ignored, entry));
+        }
+    }
+
+    public static void main(String[] args)
+        throws Exception
+    {
+        if (args.length != 1)
+        {
+            System.err.println("usage: ArtifactParser [artifacts.zip|artifacts_dir]");
+            System.exit(1);
+        }
+
+        Security.addProvider(new BouncyCastleProvider());
+        Security.addProvider(new BouncyCastlePQCProvider());
+
+        if (args[0].endsWith(".zip"))
+        {
+            processZipArtifacts(args[0]);
+        }
+        else
+        {
+            processArtifacts(args[0]);
         }
     }
 }
