405cefd6a716b0131e8cf34e3188d421cd5dec37
Alexander Railean <alexander.railean@siemens.com>
2023-04-19 16:10:37 +0200

    Add CMP server demo and instructions

--- cmp/bouncycastle/build.gradle.kts
+++ cmp/bouncycastle/build.gradle.kts
@@ -10,12 +10,6 @@ repositories {
 }
 
 dependencies {
-//    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-//    testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.bouncycastle:bcprov-jdk18on:1.73")
     implementation("org.bouncycastle:bcpkix-jdk18on:1.73")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
