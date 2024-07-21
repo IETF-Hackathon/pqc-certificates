17e7599aadfda279f75b1d907b41d2be2b9f0681
Mike Ounsworth <mike@ounsworth.ca>
2024-07-22 07:14:54 +1000

    Create a 2nd results html page for only the automated tests

--- src/test_certs_r3.sh
+++ src/test_certs_r3.sh
@@ -13,7 +13,8 @@ test_ta () {
     printf "\nTesting %s\n" $tafile
 
     # openssl always exits with 0, so we can't use exit status to tell if the cert was valid :/
-    ossl_output=$(openssl x509 -in $tafile -text -noout 2>&1)
+    ossl_output=$(openssl verify -check_ss_sig -verbose -CAfile $tafile $tafile)
+    ossl_status=$?
 
     # print it out for the logs
     echo "$ossl_output"
@@ -21,8 +22,8 @@ test_ta () {
     tafileBasename=$(basename $tafile)
     oid=${tafileBasename%_ta.pem}  # remove the suffix "_ta.pem"
 
-    # test for an error
-    if (echo $ossl_output | grep "error\|Unable\|Error" >/dev/null); then
+    # test for an error and print a link in the results CSV file
+    if [[ $ossl_status -ne 0 ]]; then
         echo "Certificate Validation Result: FAIL"
         echo $oid,N >> $resultsfile
     else
@@ -36,17 +37,17 @@ for providerdir in $(ls -d $inputdir/*/); do
     provider=$(basename $providerdir)
 
     # process certs
-    zip=$providerdir/$certszipr3
+    zip=${providerdir}$certszipr3
     printf "Unziping %s\n" $zip
     unzip -o $zip -d "artifacts_certs_r3"
 
     # Start the results CSV file
     mkdir -p $outputdir
-    resultsfile=$outputdir/${provider}_oqsprovider.csv
+    resultsfile=${outputdir}${provider}_oqsprovider.csv
     echo "key_algorithm_oid,test_result" > $resultsfile
 
     # test each TA file
-    for tafile in $(ls artifacts_certs_r3/*_ta.pem); do
+    for tafile in $(ls ${artifacts_certs_r3}*_ta.pem); do
         test_ta "$tafile" "$resultsfile"
     done
 done
