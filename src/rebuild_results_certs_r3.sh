b8a65b702440e95f458f029195546b94ad51381c
Mike Ounsworth <mike@ounsworth.ca>
2024-07-22 05:32:56 +1000

    Porting changes from ounsworth/pqc-cert-checker

--- src/rebuild_results_certs_r3.sh
+++ src/rebuild_results_certs_r3.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
 # build a list of all compat matrix files in provider dirs
-dirs=$(ls ../providers)
+dirs=$(ls providers)
 for dir in $dirs; do
-    dir=../providers/$dir
+    dir=providers/$dir
     if [ ! -d $dir ]; then continue; fi
     files+=$(find $dir/compatMatrices/artifacts_certs_r3 -name "*.csv" )
     files+=" "
 done
 
+printf "Files: %s\n" $files
+
 # build the compat matrix
-python3 pqc_report_writer_common.py ../docs/oid_mapping.md pqc_hackathon_results_certs_r3.md Certificate $files
+python3 src/pqc_report_writer_common.py docs/oid_mapping.md pqc_hackathon_results_certs_r3.md Certificate $files
 
 # convert to html
 pandoc -f markdown pqc_hackathon_results_certs_r3.md > pqc_hackathon_results_certs_r3.html
-mv pqc_hackathon_results_certs_r3.md pqc_hackathon_results_certs_r3.html oids.json ../docs
+mv pqc_hackathon_results_certs_r3.md pqc_hackathon_results_certs_r3.html docs
+
+
+# Also generate a results html with only automated test results
+python3 src/pqc_report_writer_common.py docs/oid_mapping.md pqc_hackathon_results_certs_r3_automated_tests.md Certificate $(ls ./output/certs)
+pandoc -f markdown pqc_hackathon_results_certs_r3_automated_tests.md > pqc_hackathon_results_certs_r3_automated_tests.html
+mv pqc_hackathon_results_certs_r3_automated_tests.md pqc_hackathon_results_certs_r3_automated_tests.html docs
