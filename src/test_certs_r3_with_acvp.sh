09f81d9fb4546f7fda911398c3ae4305f505ea9f
Mike Ounsworth <mike@ounsworth.ca>
2024-07-22 16:55:44 -0500

    Update test_certs_r3_with_acvp.sh

--- src/test_certs_r3_with_acvp.sh
+++ src/test_certs_r3_with_acvp.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-printf "DEBUG pwd: %s\n" $(pwd)
-printf "DEBUG ls: %s\n" $(ls)
-
 mkdir /output
-./providers/nist-acvts-tests/setup_acvts_env.sh
+/bin/sh ./providers/nist-acvts-test/setup_acvts_env.sh
