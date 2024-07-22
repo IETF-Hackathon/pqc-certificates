ba761fdc88f4fdbd2af3f443ab4f5623e06003d3
Daniel Van Geest <daniel.vangeest@cryptonext-security.com>
2024-07-22 05:53:32 +0100

    Fix CMS script with new locations

--- src/rebuild_results_cms_v1.sh
+++ src/rebuild_results_cms_v1.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # build a list of all compat matrix files in provider dirs
-dirs=$(ls ../providers)
+dirs=$(ls providers)
 for dir in $dirs; do
-    dir=../providers/$dir
+    dir=providers/$dir
     if [ ! -d $dir ]; then continue; fi
     files+=$(find $dir/compatMatrices/artifacts_cms_v1 -name "*.csv" )
     files+=" "
 done
 
 # build the compat matrix
-python3 pqc_report_writer_common.py ../docs/oid_mapping.md pqc_hackathon_results_cms_v1.md CMS $files
+python3 src/pqc_report_writer_common.py docs/oid_mapping.md pqc_hackathon_results_cms_v1.md CMS $files
 
 # convert to html
 pandoc -f markdown pqc_hackathon_results_cms_v1.md > pqc_hackathon_results_cms_v1.html
-mv pqc_hackathon_results_cms_v1.md pqc_hackathon_results_cms_v1.html oids.json ../docs
+mv pqc_hackathon_results_cms_v1.md pqc_hackathon_results_cms_v1.html docs
