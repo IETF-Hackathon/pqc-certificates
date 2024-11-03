e0a65123b05153c964427706d727b760d6b5a435
David Hook <dgh@cryptoworkshop.com>
2024-11-03 21:08:03 +1100

    removed javac!

--- providers/bc/verify_r3.sh
+++ providers/bc/verify_r3.sh
@@ -7,6 +7,4 @@ cd $SCRIPT_DIR
 
 base=`cat lib/beta.ver`
 
-javac -d classes -cp lib/bcprov-${base}.jar:lib/bcutil-${base}.jar:lib/bcpkix-${base}.jar src/main/java/*.java
-
 java -cp classes:lib/bcprov-${base}.jar:lib/bcutil-${base}.jar:lib/bcpkix-${base}.jar Verify self-signed $1
