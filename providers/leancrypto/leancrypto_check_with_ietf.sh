c80f25429b9b76730ba3d9b19fff58356764cc33
smuellerDD <smueller@chronox.de>
2026-01-26 00:10:18 +0100

    leancrypto certificate artifacts v5 (#257)
    Add support for testing all current providers. Testing generally includes
    
    - parsing of X.509 certificate and duming its contents
    
    - generating a PKCS#7 blob using the provider's certificate and secret key
      signing the certificate - this verifies the parsing of the private key
    
    - verifying the PKCS#7 blobk using the provider's certificate including
      a full trust verfification
    
    The following shortcomings are evident with the testing and thus not all of
    the general testing steps are performed for all providers:
    
    - crypto4a: Composite certificates are not self-signed
            -> cannot verify signatures in my implementation as a certificate with X509v3 Subject Key Identifier == X509v3 Authority Key Identifier must be self-signed in leancrypto to act as root of certificate chain - otherwise a chain loop is detected
    
    - BC: Composite certificates have missing SKID in the certificates, my certificate handling code rejected all certificates.
    
    - corey_digicert: All PKCS#8 blobs are of PKCS8 version 2. My PKCS#8 parser only supports
     version 1 at this point. Thus, checks only cover the parsing of the certificate.
    
    - entrust: Verification of signature was generally successful, but due to missing keyCert
    Sign key usage in X.509 certificates, the full validation failed.
    
    - openjdk: due to missing SKID in the certificates, my certificate handling code rejected
     all certificates.
    
    - safelogic: failure to parse ml-dsa-65-ed25519-sha512-1.3.6.1.5.5.7.6.48_priv.der: the provided data is not 64 bytes (or even 96 bytes if your ED25519 secret key has the full key size fo 64 bytes). It looks as if the full ML-DSA secret key instead of just the seed is present
    
    - composite-sigs-ref-impl: due to missing SKID in the certificates, my certificate handling code rejected all certificates.
    
    Signed-off-by: Stephan Mueller <smueller@chronox.de>

--- providers/leancrypto/leancrypto_check_with_ietf.sh
+++ providers/leancrypto/leancrypto_check_with_ietf.sh
@@ -9,8 +9,8 @@
 # To utilize this script, perform the following steps:
 #
 # 1. compile leancrypto with X.509 generator enabled
-# 2. update variable LC_X509_CHECKER below to point to the lc_x509_generator
-#    tool
+# 2. update variables LC_X509_CHECKER and LC_PKCS7_CHECKER below to point to the
+#    lc_x509_generator and lc_pkcs7_generator tool path
 # 3. cd into the https://github.com/IETF-Hackathon/pqc-certificates/providers
 #    directory
 # 4. Execute this script
@@ -19,8 +19,49 @@
 #
 
 LC_X509_CHECKER="lc_x509_generator"
+LC_PKCS7_CHECKER="lc_pkcs7_generator"
 
-TMPDIR="./tmp"
+if [ -n "$1" ]
+then
+	LC_X509_CHECKER=$1
+fi
+
+if [ ! -x "$LC_X509_CHECKER" ]
+then
+	exit 77
+fi
+
+if [ -n "$2" ]
+then
+	LC_PKCS7_CHECKER=$2
+fi
+
+if [ ! -x "$LC_PKCS7_CHECKER" ]
+then
+	exit 77
+fi
+
+
+SRCDIR="."
+if [ -n "$3" ]
+then
+	SRCDIR=$3
+fi
+
+#check presence of unzip
+unzip > /dev/null 2>&1
+if [ $? -ne 0 ]
+then
+	exit 0
+fi
+
+TMPDIR="./tmp.$$"
+
+STATUSOUT="$TMPDIR"
+if [ -n "$4" ]
+then
+	STATUSOUT=$4
+fi
 
 global_failure_count=0
 
@@ -66,24 +107,166 @@ echo_info()
 	echo $(color "magenta")[INFO]$(color off) "$@"
 }
 
-check_one() {
-	local inputfile=$1
+statusfile=""
+doc_init() {
+	local outputfile=$1
 
-	if [ ! -f "$inputfile" ]
+	echo "key_algorithm_oid,type,test_result" > $outputfile
+	statusfile="$outputfile"
+}
+
+doc_one() {
+	local oid=$1
+	local type=$2 #both, expandedkey, seed, priv
+	local res=$3 #Y, N
+
+	touch $statusfile
+	echo "$oid,$type,$res" >> $statusfile
+}
+
+lc_sign_cert() {
+	local pk_file=$1
+	local sk_file=$2
+
+	echo_info "Leancrypto: Create PKCS#7 signature of X.509 certificate using the X.509 certificate and associated PKCS#8 private key as signer"
+
+	$LC_PKCS7_CHECKER \
+	 --md SHA2-512 \
+	 -i ${pk_file} \
+	 -o ${pk_file}.p7b \
+	 --x509-signer  ${pk_file} \
+	 --signer-sk-file ${sk_file}
+
+	if [ $? -ne 0 ]
 	then
-		echo_fail "Cannot find $inputfile"
+		echo_fail "Failed leancrypto signature generation"
+		return 1
+	else
+		echo_success "Successful leancrypto signature generation"
+		return 0
+	fi
+}
+
+lc_verify_cert() {
+	local pk_file=$1
+
+	echo_info "Leancrypto: Verify PKCS#7 signature of X.509 certificate using the X.509 certificate and associated PKCS#8 private key as signer"
+
+	$LC_PKCS7_CHECKER \
+	 --print-pkcs7 ${pk_file}.p7b \
+	 -i ${pk_file} \
+	 --trust-anchor ${pk_file}
+
+	if [ $? -ne 0 ]
+	then
+		echo_fail "Failed leancrypto signature verification"
+		return 1
+	else
+		echo_success "Successful leancrypto signature verification"
+		return 0
+	fi
+}
+
+check_one() {
+	local certfile=$1
+	local certcheck_only=$2
+	local oid=$3
+	local type="priv"
+
+	if [ ! -f "$certfile" ]
+	then
+		echo_fail "Cannot find $certfile"
 		exit 1
 	fi
 
-	echo "=== File $inputfile ==="
-	$LC_X509_CHECKER --print-x509 $inputfile
+	echo "=== File $certfile ==="
+	$LC_X509_CHECKER --print-x509 $certfile
+	#$LC_X509_CHECKER --check-selfsigned --print-x509 $certfile
 	if [ $? -ne 0 ]
 	then
-		echo_fail "Parsing of file $inputfile was unsuccessful"
+		echo_fail "Parsing of file $certfile was unsuccessful"
 		global_failure_count=$(($global_failure_count+1))
-	else
-		echo_success "Parsing of file $inputfile was successful"
+		if [ -n "$oid" ]
+		then
+			doc_one "$oid" "$type" "N"
+		fi
+		return
 	fi
+
+	if [ $certcheck_only -eq 1 ]
+	then
+		echo_success "Parsing of file $certfile was successful"
+		if [ -n "$oid" ]
+		then
+			doc_one "$oid" "$type" "Y"
+		fi
+		return
+	fi
+
+	local privkey_part=${certfile%%_ta.der}
+
+	for i in "priv" "seed_priv" "seed" "both_priv" "expandedkey_priv"
+	do
+		local privkey="${privkey_part}_${i}.der"
+		if [ ! -f "$privkey" ]
+		then
+			echo_info "Cannot find private key $privkey for $certfile"
+			continue
+		fi
+
+		if [ x"$i" = x"priv" ]
+		then
+			type="priv"
+		elif [ x"$i" = x"seed_priv" ]
+		then
+			type="seed"
+		elif [ x"$i" = x"seed" ]
+		then
+			type="seed"
+		elif [ x"$i" = x"both_priv" ]
+		then
+			type="both"
+		elif [ x"$i" = x"expandedkey_priv" ]
+		then
+			type="expandedkey"
+		fi
+
+		rm -f ${certfile}.p7b
+
+		lc_sign_cert $certfile $privkey
+		if [ $? -ne 0 ]
+		then
+			echo_fail "Generating signature of file $certfile was unsuccessful"
+			global_failure_count=$(($global_failure_count+1))
+			if [ -n "$oid" ]
+			then
+				doc_one "$oid" "$type" "N"
+			fi
+			continue
+		fi
+
+		lc_verify_cert $certfile
+
+		# We try to validate, but only print the output without logging
+		# the result - this is due to the fact that some provider like
+		# CHT do not include the root CA.
+# 		if [ $? -ne 0 ]
+# 		then
+# 			echo_fail "Verifying signature of file $certfile was unsuccessful"
+# 			global_failure_count=$(($global_failure_count+1))
+# 			if [ -n "$oid" ]
+# 			then
+# 				doc_one "$oid" "$type" "N"
+# 			fi
+# 			continue
+# 		fi
+
+		echo_success "Parsing of file $certfile was successful"
+		if [ -n "$oid" ]
+		then
+			doc_one "$oid" "$type" "Y"
+		fi
+	done
 }
 
 report_result() {
@@ -99,7 +282,8 @@ report_result() {
 }
 
 extract_files() {
-	local zipfile=$1
+	local provider=$(dirname $1)
+	local zipfile="${SRCDIR}/$1"
 
 	if [ ! -f "$zipfile" ]
 	then
@@ -110,143 +294,186 @@ extract_files() {
 	rm -rf $TMPDIR
 	mkdir $TMPDIR
 	unzip $zipfile -d $TMPDIR
+
+	if [ ! -d $STATUSOUT ]
+	then
+		mkdir $STATUSOUT
+		if [ $? -ne 0 ]
+		then
+			echo_fail "Cannot create $STATUSOUT"
+			exit 1
+		fi
+	fi
+	doc_init $STATUSOUT/${provider}_leancrypto.csv
 }
 
+ml_dsa_oids="2.16.840.1.101.3.4.3.17 2.16.840.1.101.3.4.3.18 2.16.840.1.101.3.4.3.19"
+composite_ml_dsa_oids="1.3.6.1.5.5.7.6.39 1.3.6.1.5.5.7.6.48 1.3.6.1.5.5.7.6.51"
+slh_dsa_oids="2.16.840.1.101.3.4.3.26 2.16.840.1.101.3.4.3.27 2.16.840.1.101.3.4.3.28 2.16.840.1.101.3.4.3.29 2.16.840.1.101.3.4.3.30 2.16.840.1.101.3.4.3.31"
+
 bc_test() {
-	local slh_dsa_files="slh-dsa-shake-128f-2.16.840.1.101.3.4.3.27_ta.der slh-dsa-shake-128s-2.16.840.1.101.3.4.3.26_ta.der slh-dsa-shake-192f-2.16.840.1.101.3.4.3.29_ta.der slh-dsa-shake-192s-2.16.840.1.101.3.4.3.28_ta.der slh-dsa-shake-256f-2.16.840.1.101.3.4.3.31_ta.der slh-dsa-shake-256s-2.16.840.1.101.3.4.3.30_ta.der"
-	local composite_ml_dsa_files="MLDSA44-Ed25519-SHA512-2.16.840.1.114027.80.8.1.23_ta.der MLDSA65-Ed25519-SHA512-2.16.840.1.114027.80.8.1.30_ta.der"
-	local ml_dsa_files="ml-dsa-44-2.16.840.1.101.3.4.3.17_ta.der ml-dsa-65-2.16.840.1.101.3.4.3.18_ta.der ml-dsa-87-2.16.840.1.101.3.4.3.19_ta.der"
+	local oids="$ml_dsa_oids $slh_dsa_oids"
+	#local oids="$ml_dsa_oids $composite_ml_dsa_oids $slh_dsa_oids"
+	local file=""
 
-	local files="$slh_dsa_files $composite_ml_dsa_files $ml_dsa_files"
-
-	extract_files "bc/artifacts_certs_r4.zip"
-	for i in $files
+	extract_files "bc/artifacts_certs_r5.zip"
+	for i in $oids
 	do
-		check_one "$TMPDIR/$i"
+		file=$TMPDIR/*$i*_ta.der
+		for j in $file
+		do
+			echo $j | grep -q "catalyst" && continue
+			echo $j | grep -q "chameleon" && continue
+			echo $j | grep -q "composite_1.2.840" && continue
+
+			# TODO set 1 -> 0 when certificates received an SKID
+			check_one $j 1 $i
+		done
 	done
 }
 
 redhound_test() {
-	local subdir="artifacts_certs_v4"
-	local slh_dsa_files="$subdir/slh-dsa-shake-128f-2.16.840.1.101.3.4.3.27_ta.der $subdir/slh-dsa-shake-128s-2.16.840.1.101.3.4.3.26_ta.der $subdir/slh-dsa-shake-192f-2.16.840.1.101.3.4.3.29_ta.der $subdir/slh-dsa-shake-192s-2.16.840.1.101.3.4.3.28_ta.der $subdir/slh-dsa-shake-256f-2.16.840.1.101.3.4.3.31_ta.der $subdir/slh-dsa-shake-256s-2.16.840.1.101.3.4.3.30_ta.der"
-	local ml_dsa_files="$subdir/ml-dsa-44-2.16.840.1.101.3.4.3.17_ta.der $subdir/ml-dsa-65-2.16.840.1.101.3.4.3.18_ta.der $subdir/ml-dsa-87-2.16.840.1.101.3.4.3.19_ta.der"
+	local subdir="artifacts_certs_r5"
+	local oids="$ml_dsa_oids $slh_dsa_oids"
 
-	local files="$slh_dsa_files $ml_dsa_files"
-
-	extract_files "carl-redhound/artifacts_certs_r4.zip"
-	for i in $files
+	extract_files "carl-redhound/artifacts_certs_r5.zip"
+	for i in $oids
 	do
-		check_one "$TMPDIR/$i"
+		check_one $TMPDIR/$subdir/*$i*_ta.der 0 $i
 	done
 }
 
 cht_test() {
-	local subdir="artifacts_certs_r4"
-	local slh_dsa_files="$subdir/SLH-DSA-SHAKE-128f-2.16.840.1.101.3.4.3.27_ta.der $subdir/SLH-DSA-SHAKE-128s-2.16.840.1.101.3.4.3.26_ta.der $subdir/SLH-DSA-SHAKE-192f-2.16.840.1.101.3.4.3.29_ta.der $subdir/SLH-DSA-SHAKE-192s-2.16.840.1.101.3.4.3.28_ta.der $subdir/SLH-DSA-SHAKE-256f-2.16.840.1.101.3.4.3.31_ta.der $subdir/SLH-DSA-SHAKE-256s-2.16.840.1.101.3.4.3.30_ta.der"
-	local composite_ml_dsa_files="$subdir/id-MLDSA44-Ed25519-2.16.840.1.114027.80.8.1.23_ta.der $subdir/id-MLDSA65-Ed25519-2.16.840.1.114027.80.8.1.30_ta.der"
-	local ml_dsa_files="$subdir/ML-DSA-44-2.16.840.1.101.3.4.3.17_ta.der $subdir/ML-DSA-65-2.16.840.1.101.3.4.3.18_ta.der $subdir/ML-DSA-87-2.16.840.1.101.3.4.3.19_ta.der"
+	local subdir="./"
+	local oids="$ml_dsa_oids $composite_ml_dsa_oids $slh_dsa_oids"
 
-	local files="$slh_dsa_files $composite_ml_dsa_files $ml_dsa_files"
-
-	extract_files "cht/artifacts_certs_r4.zip"
-	for i in $files
+	extract_files "cht/artifacts_certs_r5.zip"
+	for i in $oids
 	do
-		check_one "$TMPDIR/$i"
+		check_one $TMPDIR/$subdir/*$i*_ta.der 0 $i
 	done
 }
 
 corey_digicert_test() {
-	local ml_dsa_files="ML-DSA-44-2.16.840.1.101.3.4.3.17_ta.der ML-DSA-65-2.16.840.1.101.3.4.3.18_ta.der ML-DSA-87-2.16.840.1.101.3.4.3.19_ta.der"
+	local oids="$ml_dsa_oids"
 
-	local files="$ml_dsa_files"
-
-	extract_files "corey-digicert/artifacts_certs_r4.zip"
-	for i in $files
+	extract_files "corey-digicert/artifacts_certs_r5.zip"
+	for i in $oids
 	do
-		check_one "$TMPDIR/$i"
+		# TODO set 1 -> when either the PKCS#8 blobs are version 1
+		# or leancrypto received support for version 2
+		check_one $TMPDIR/$subdir/*$i*_ta.der 1 $i
+	done
+}
+
+crypto4a_test() {
+	local oids="$ml_dsa_oids $composite_ml_dsa_oids $slh_dsa_oids"
+
+	extract_files "crypto4a/artifacts_certs_r5.zip"
+	for i in $oids
+	do
+		check_one $TMPDIR/*$i*_ta.der 0 $i
 	done
 }
 
 cryptonext_test() {
-	local subdir="artifacts_certs_r4"
-	local slh_dsa_files="$subdir/SLH-DSA-SHAKE-128f-2.16.840.1.101.3.4.3.27_ta.der $subdir/SLH-DSA-SHAKE-128s-2.16.840.1.101.3.4.3.26_ta.der $subdir/SLH-DSA-SHAKE-192f-2.16.840.1.101.3.4.3.29_ta.der $subdir/SLH-DSA-SHAKE-192s-2.16.840.1.101.3.4.3.28_ta.der $subdir/SLH-DSA-SHAKE-256f-2.16.840.1.101.3.4.3.31_ta.der $subdir/SLH-DSA-SHAKE-256s-2.16.840.1.101.3.4.3.30_ta.der"
-	local ml_dsa_files="$subdir/ML-DSA-44-2.16.840.1.101.3.4.3.17_ta.der $subdir/ML-DSA-65-2.16.840.1.101.3.4.3.18_ta.der $subdir/ML-DSA-87-2.16.840.1.101.3.4.3.19_ta.der"
+	local subdir="artifacts_certs_r5"
+	local oids="$ml_dsa_oids $composite_ml_dsa_oids $slh_dsa_oids"
 
-	local files="$slh_dsa_files $ml_dsa_files"
-
-	extract_files "cryptonext/artifacts_certs_r4.zip"
-	for i in $files
+	extract_files "cryptonext/artifacts_certs_r5.zip"
+	for i in $oids
 	do
-		check_one "$TMPDIR/$i"
-	done
-}
-
-cryptonext_cnsprovider_test() {
-	local subdir="artifacts_certs_r4"
-	local slh_dsa_files="$subdir/SLH-DSA-SHAKE-128f-2.16.840.1.101.3.4.3.27_ta.der $subdir/SLH-DSA-SHAKE-128s-2.16.840.1.101.3.4.3.26_ta.der $subdir/SLH-DSA-SHAKE-192f-2.16.840.1.101.3.4.3.29_ta.der $subdir/SLH-DSA-SHAKE-192s-2.16.840.1.101.3.4.3.28_ta.der $subdir/SLH-DSA-SHAKE-256f-2.16.840.1.101.3.4.3.31_ta.der $subdir/SLH-DSA-SHAKE-256s-2.16.840.1.101.3.4.3.30_ta.der"
-	local ml_dsa_files="$subdir/ML-DSA-44-2.16.840.1.101.3.4.3.17_ta.der $subdir/ML-DSA-65-2.16.840.1.101.3.4.3.18_ta.der $subdir/ML-DSA-87-2.16.840.1.101.3.4.3.19_ta.der"
-
-	local files="$slh_dsa_files $ml_dsa_files"
-
-	extract_files "cryptonext-cnsprovider/artifacts_certs_r4.zip"
-	for i in $files
-	do
-		check_one "$TMPDIR/$i"
+		check_one $TMPDIR/$subdir/*$i*_ta.der 0 $i
 	done
 }
 
 entrust_test() {
-	local subdir="artifacts_certs_r4"
-	local ml_dsa_files="$subdir/ML-DSA-44-2.16.840.1.101.3.4.3.17_ta.der $subdir/ML-DSA-44-2.16.840.1.101.3.4.3.18_ta.der $subdir/ML-DSA-44-2.16.840.1.101.3.4.3.19_ta.der"
+	local subdir="artifacts"
+	local oids="$ml_dsa_oids $composite_ml_dsa_oids $slh_dsa_oids"
 
-	local files="$ml_dsa_files"
-
-	extract_files "entrust/artifacts_certs_r4.zip"
-	for i in $files
+	extract_files "entrust/artifacts_certs_r5.zip"
+	for i in $oids
 	do
-		check_one "$TMPDIR/$i"
+		check_one $TMPDIR/$subdir/*$i*_ta.der 0 $i
 	done
 }
 
 kris_test() {
 	local subdir="artifacts_certs_r4"
-	local ml_dsa_files="$subdir/ml-dsa-44-2.16.840.1.101.3.4.3.17_ta.der $subdir/ml-dsa-65-2.16.840.1.101.3.4.3.18_ta.der $subdir/ml-dsa-87-2.16.840.1.101.3.4.3.19_ta.der"
-
-	local files="$ml_dsa_files"
+	local oids="$ml_dsa_oids"
 
 	extract_files "kris/artifacts_certs_r4.zip"
-	for i in $files
+	for i in $oids
 	do
-		check_one "$TMPDIR/$i"
+		check_one $TMPDIR/$subdir/*$i*_ta.der 0 $i
+	done
+}
+
+openjdk_test() {
+	local oids="$ml_dsa_oids"
+
+	extract_files "openjdk/artifacts_certs_r5.zip"
+	for i in $oids
+	do
+		# TODO set 1 -> 0 when certificates received an SKID
+		check_one $TMPDIR/*$i*_ta.der 1 $i
+	done
+}
+
+openssl_test() {
+	local subdir="./"
+	local oids="$ml_dsa_oids $slh_dsa_oids"
+
+	extract_files "openssl/artifacts_certs_r5.zip"
+	for i in $oids
+	do
+		check_one $TMPDIR/$subdir/*$i*_ta.der 0 $i
 	done
 }
 
 seventhsense_test() {
-	local slh_dsa_files="SlhDsaShake128f-2.16.840.1.101.3.4.3.27_ta.der SlhDsaShake128s-2.16.840.1.101.3.4.3.26_ta.der SlhDsaShake192f-2.16.840.1.101.3.4.3.29_ta.der SlhDsaShake192s-2.16.840.1.101.3.4.3.28_ta.der SlhDsaShake256f-2.16.840.1.101.3.4.3.31_ta.der SlhDsaShake256s-2.16.840.1.101.3.4.3.30_ta.der"
-	local composite_ml_dsa_files="MlDsa44Ed25519-2.16.840.1.114027.80.8.1.23_ta.der MlDsa65Ed25519-2.16.840.1.114027.80.8.1.30_ta.der"
-	local ml_dsa_files="MlDsa44-2.16.840.1.101.3.4.3.17_ta.der MlDsa65-2.16.840.1.101.3.4.3.18_ta.der MlDsa87-2.16.840.1.101.3.4.3.19_ta.der"
-
-	local files="$slh_dsa_files $composite_ml_dsa_files $ml_dsa_files"
+	#local oids="$ml_dsa_oids $composite_ml_dsa_oids $slh_dsa_oids"
+	local oids="$ml_dsa_oids $slh_dsa_oids"
 
 	extract_files "seventhsense.ai/artifact_certs_r4.zip"
-	for i in $files
+	for i in $oids
 	do
-		check_one "$TMPDIR/$i"
+		check_one $TMPDIR/*$i*_ta.der 0 $i
 	done
 }
 
-leancrypto_test() {
-	local subdir="artifacts_certs_r4"
-	local slh_dsa_files="$subdir/SLH-DSA-SHAKE-128F-2.16.840.1.101.3.4.3.27_ta.der $subdir/SLH-DSA-SHAKE-128S-2.16.840.1.101.3.4.3.26_ta.der $subdir/SLH-DSA-SHAKE-192F-2.16.840.1.101.3.4.3.29_ta.der $subdir/SLH-DSA-SHAKE-192S-2.16.840.1.101.3.4.3.28_ta.der $subdir/SLH-DSA-SHAKE-256F-2.16.840.1.101.3.4.3.31_ta.der $subdir/SLH-DSA-SHAKE-256S-2.16.840.1.101.3.4.3.30_ta.der"
-	local composite_ml_dsa_files="$subdir/ML-DSA44-ED25519-SHA512-2.16.840.1.114027.80.8.1.23_ta.der $subdir/ML-DSA65-ED25519-SHA512-2.16.840.1.114027.80.8.1.30_ta.der"
-	local ml_dsa_files="$subdir/ML-DSA44-2.16.840.1.101.3.4.3.17_ta.der $subdir/ML-DSA65-2.16.840.1.101.3.4.3.18_ta.der $subdir/ML-DSA87-2.16.840.1.101.3.4.3.19_ta.der"
+safelogic_test() {
+	local oids="$ml_dsa_oids $slh_dsa_oids"
 
-	local files="$slh_dsa_files $composite_ml_dsa_files $ml_dsa_files"
-
-	extract_files "leancrypto/artifacts_certs_r4.zip"
-	for i in $files
+	extract_files "safelogic/artifacts_certs_r5.zip"
+	for i in $oids
 	do
-		check_one "$TMPDIR/$i"
+		check_one $TMPDIR/*$i*_ta.der 0 $i
+	done
+
+	# TODO set 1 -> 0 when private key complies with draft (64 bytes size only)
+	check_one $TMPDIR/$subdir/*1.3.6.1.5.5.7.6.48*_ta.der 1 1.3.6.1.5.5.7.6.48
+}
+
+leancrypto_test() {
+	local subdir="artifacts_certs_r5"
+	local oids="$ml_dsa_oids $composite_ml_dsa_oids $slh_dsa_oids"
+
+	extract_files "leancrypto/artifacts_certs_r5.zip"
+	for i in $oids
+	do
+		check_one $TMPDIR/$subdir/*$i*_ta.der 1 $i
+	done
+}
+
+composite_ref_imp_test() {
+	local oids="$ml_dsa_oids $composite_ml_dsa_oids"
+
+	extract_files "composite-ref-impl/artifacts_certs_r5.zip"
+	for i in $oids
+	do
+		# TODO set 1 -> 0 when certificates received an SKID
+		check_one $TMPDIR/$subdir/*$i*_ta.der 1 $i
 	done
 }
 
@@ -254,11 +481,15 @@ bc_test
 redhound_test
 cht_test
 corey_digicert_test
+crypto4a_test
 cryptonext_test
-cryptonext_cnsprovider_test
 entrust_test
 kris_test
+openjdk_test
+openssl_test
 seventhsense_test
+safelogic_test
 leancrypto_test
+composite_ref_imp_test
 
 report_result
