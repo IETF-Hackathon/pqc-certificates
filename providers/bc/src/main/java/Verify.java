db9976d2a0a3f7dc2c1be6bcf5051f1cca63364d
David Hook <dghgit@cryptoworkshop.com>
2026-01-30 02:01:14 +1100

    Additional patches to R5 Parser to deal with KEM artifacts that are missing _priv or certificate extensions (#259)
    * updated for BC beta
    
    * updated R4 artifacts
    
    * removed javac!
    
    * inital R5 artifacts - composite still old-style
    
    * added signing check for ML-DSA/SLH-DSA private keys.
    
    * correct name for _both_priv encodings
    
    * updated composite signature names
    
    * set to 1.83
    
    * added support for zip file, modified to output CSV line
    
    * added null check to prevent failure if test certificate has no extensions.
    
    * added checking for expanded and both for ML-KEM keys.

--- providers/bc/src/main/java/Verify.java
+++ providers/bc/src/main/java/Verify.java
@@ -1,12 +1,17 @@
 import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
-import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.security.Security;
+import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import org.bouncycastle.cert.X509CertificateHolder;
-import org.bouncycastle.jcajce.provider.asymmetric.X509;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.openssl.PEMParser;
 import org.bouncycastle.util.io.Streams;
@@ -29,40 +34,44 @@ public class Verify
             byte[] data = null;
             if (args[1].endsWith(".pem"))
             {
-                PEMParser pemPrs = new PEMParser(new FileReader(args[1]));
-
-                X509CertificateHolder hld = (X509CertificateHolder)pemPrs.readObject();
-
-                pemPrs.close();
-
-                data = hld.getEncoded();
+                FileInputStream input = new FileInputStream(args[1]);
+                parsePem(certFact, args[1], input);
+                input.close();
             }
             else if (args[1].endsWith(".der"))
             {
-                FileInputStream fIn = new FileInputStream((args[1]));
-
-                data = Streams.readAll(fIn);
-
-                fIn.close();
+                FileInputStream input = new FileInputStream(args[1]);
+                parseDer(certFact, args[1], input);
+                input.close();
+            }
+            else if (args[1].endsWith(".zip"))
+            {
+                ZipFile zipFile = new ZipFile(args[1]);
+                for (Enumeration<? extends ZipEntry> en = zipFile.entries(); en.hasMoreElements(); )
+                {
+                    ZipEntry entry = en.nextElement();
+                    String zipName = entry.getName();
+                    if (zipName.contains("chameleon")
+                        || zipName.contains("catalyst")
+                        || zipName.contains("composite"))     // filter out old names
+                    {
+                        continue;
+                    }
+                    
+                    if (zipName.contains("_ta") && zipName.endsWith(".pem"))
+                    {
+                        parsePem(certFact, zipName, zipFile.getInputStream(entry));
+                    }
+                    else if (zipName.contains("_ta") && zipName.endsWith(".der"))
+                    {
+                        parseDer(certFact, zipName, zipFile.getInputStream(entry));
+                    }
+                }
+                zipFile.close();
             }
             else
             {
                 System.out.println("unknown file: " + args[1]);
-                System.exit(1);
-            }
-
-            X509Certificate cert = (X509Certificate)certFact.generateCertificate(new ByteArrayInputStream(data));
-
-            try
-            {
-                cert.verify(cert.getPublicKey());
-
-                System.out.println(args[1] + " verified");
-                System.exit(0);
-            }
-            catch (Exception e)
-            {
-                System.out.println(args[1] + " failed verification");
                 System.exit(1);
             }
         }
@@ -72,6 +81,58 @@ public class Verify
         }
 
         System.exit(1);
+    }
+
+    private static String createResultName(String fileName)
+    {
+        String oid = fileName.substring(fileName.lastIndexOf('-') + 1);
+        oid = oid.substring(0, oid.indexOf("_"));
+        return oid;
+    }
+
+    private static void parsePem(CertificateFactory certFact, String arg, InputStream input)
+        throws IOException, CertificateException
+    {
+        PEMParser pemPrs = new PEMParser(new InputStreamReader(input));
+
+        X509CertificateHolder hld = (X509CertificateHolder)pemPrs.readObject();
+
+        pemPrs.close();
+
+        byte[] data = hld.getEncoded();
+
+        X509Certificate cert = (X509Certificate)certFact.generateCertificate(new ByteArrayInputStream(data));
+
+        String oid = createResultName(arg);
+        try
+        {
+            cert.verify(cert.getPublicKey());
+
+            System.out.println(oid + ",cert,Y");
+        }
+        catch (Exception e)
+        {
+            System.out.println(oid + ",cert,N");
+        }
+    }
+
+    private static void parseDer(CertificateFactory certFact, String arg, InputStream input)
+        throws IOException, CertificateException
+    {
+        byte[] data = Streams.readAll(input);
+
+        X509Certificate cert = (X509Certificate)certFact.generateCertificate(new ByteArrayInputStream(data));
+        String oid = createResultName(arg);
+        try
+        {
+            cert.verify(cert.getPublicKey());
+
+            System.out.println(oid + ",cert,Y");
+        }
+        catch (Exception e)
+        {
+            System.out.println(oid + ",cert,N");
+        }
     }
 }
 
